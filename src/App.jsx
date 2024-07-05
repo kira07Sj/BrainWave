a7c741015c2a249438e80c1695db32db73c6b067
kira07sj <kirubeldagne1@gmail.com>
2024-07-04 23:18:22 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import ButtonGradient from './assets/svg/ButtonGradient';
+import Button from './components/Button';
+import Header from './components/Header';
+
+function App() {
+
+
+  return (
+    <>
+    
+    
+
+    <ButtonGradient/>
+    </>
+  );
+}
+
+export default App
