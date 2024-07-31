8adeb738159223649d2deb3b5b6ffb5c77dec33c
kira07sj <kirubeldagne1@gmail.com>
2024-07-31 00:41:37 -0700

    the last service card completed

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -62,7 +62,39 @@ const Services = () => {
                    <PhotoChatMessage/>
 
                </div>
+
+              <div className="p-4 bg-n-7 rounded-3xl border border-n-1/10
+              overflow-hidden lg:min-h-[46rem]">
+                <div className=" py-12 px-4 xl:px-8">
+                  <h4 className="h4 mb-4">Video Generation</h4>
+                  <p className=" body-2 text-n-3 mb-8">The world's most powerful AI photo and video 
+                    art generation engine. What will you create?
+                  </p>
+                  <ul className=" flex items-center justify-between">
+                    {brainwaveServicesIcons.map((item, index) =>(
+                      <li className={`flex items-center justify-center
+                       ${index===2 ? 'w-[3rem] h-[3rem] p-0.25 bg-conic-gradient rounded-2xl md:w-[4.5rem] md:h-[4.5rem]'
+                        : ' flex w-10 h-10 bg-n-6 rounded-2xl md:w-15 md:h-15 '}`} key={index}>
+                        <div className={
+                          index===2 ?
+                         "flex items-center justify-center bg-n-7 w-full h-full rounded-[1rem]"
+                          : ''
+                        }><img src={item} width={24} height={24} alt="icons" /></div>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+
+                <div className=" relative h-[20rem] bg-n-8 rounded-xl overflow-hidden md:h-[25rem]
+                 ">
+                  <img className="w-full h-full object-cover" 
+                  width={520} height={400} src={service3} alt="robot2" />
+                  <VideoChatMessage/>
+                  <VideoBar/>
+                </div>
+              </div>
             </div>
+            <Gradient/>
 
           
               
