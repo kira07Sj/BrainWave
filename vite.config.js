3006ef76619723ac3fb2ec16d731c65eaed73704
kira07sj <kirubeldagne1@gmail.com>
2024-11-30 14:42:26 +0300

    Remove: base router

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 
-  base: '/BrainWave/', // Set the base path for the application
+  
   build: {
     outDir: 'dist', // Ensure the output directory matches your Render publish directory
   },
