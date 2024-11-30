0e57b82d426b10068d6606fe431103d48c4de40b
kira07sj <kirubeldagne1@gmail.com>
2024-11-30 20:27:09 +0300

    Update: Responsiveness

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -13,7 +13,7 @@ const Benefits = () => {
             <Heading className="md:max-w-md lg:max-w-2xl  " title="Chat Smarter, Not Harder with Brainwave"></Heading>
         </div>
 
-        <div className=" flex flex-wrap  gap-10  p-3 mx-[3rem]  mb-10 justify-center">
+        <div className=" flex flex-wrap  gap-10  p-3 mx-[3rem] max-md:mx-0  mb-10 justify-center">
             {benefits.map((cards) =>(
                 <div className="block relative p-0.5 bg-no-reapt
                  bg-[length:100%_100%]  md:max-w-[24rem]  rounded-3xl "
